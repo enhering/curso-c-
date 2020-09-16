59efde10ede736f432bd31b841347827e5c4d3bb
enhering <e.n.hering@icloud.com>
2020-09-16 18:05:28 -0300

    Mudei mais

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,5 @@
 
 int main() {
   std::cout << "Hello World!" << std::endl;
-  std::cout << "Mais uma coisa!"
-            << "Nào sei mais o que escrever"
-            << std::endl;
-
-  int nInteiro = 5;
-
-  std::cout << "O meu número inteiro é " << nInteiro << std::endl;
-
 }
+
