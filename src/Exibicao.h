e7df3255ad5c79344f44043b6581a2ad637eb320
enhering <e.n.hering@icloud.com>
2020-09-23 18:30:55 -0300

    Bibliotecas criadas

--- src/Exibicao.h
+++ src/Exibicao.h
@@ -1,3 +1,9 @@
+#ifndef __EXIBICAO_H
+  #define __EXIBICAO_H
+
+
 #include <iostream>
 
 void MostrarIdade(int);
+
+#endif
--- src/Operacoes.h
+++ src/Operacoes.h
@@ -1,2 +1,7 @@
+#ifndef __OPERACOES_C
+  #define __OPERACOES_C
+
 int Soma(int, int);
 int Subtacao(int, int);
+
+#endif
